80419c20951f6051e573120def14fc1e449e4cfb
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2022-02-23 20:35:39 -0300

    First commit

---
+++ csharp-paralelismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_paralelismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
